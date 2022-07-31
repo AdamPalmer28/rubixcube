286a0ad6d29216c6ef8622131c6969a1e54168f7
AdamPalmer28 <93833080+AdamPalmer28@users.noreply.github.com>
2022-07-31 19:50:27 +0100

    Rubix cube logic complete

--- rubix.py
+++ rubix.py
@@ -1,8 +1,9 @@
 import numpy as np
+import copy
 
-class rubix:
+class rubix_logic:
     """
-    Handles rubix cube logic arround rotations 
+    Handles rubix cube logic - specifically rotations
 
     Colours:
         0 - White
@@ -13,12 +14,12 @@ class rubix:
         5 - Yellow
 
     Completed cube net:
-        (for visualisation)
-        -   -   -   -
-        -   4   -   -
-        2 . 0 . 3 . 5
-        -   1   -   -
-        -   -   -   -
+        (with array direction)
+        - -   - -   - -   - -
+        - -   4 v   - -   - -
+        2 <   0 <   3 <   5 <
+        - -   1 ^   - -   - -
+        - -   - -   - -   - -  
 
     Cube rotations:
         xy - rotation parallel to 0 / 5 (white / yellow)
@@ -30,43 +31,92 @@ class rubix:
     def __init__(self, n: int):
 
         self.n = n 
-
-        self.sides = []
-        for i in range(6):
-            self.sides.append( np.repeat(i, self.n**2 ).reshape( self.n, self.n) )
-
-        self.test_array = np.array( [[1, 2, 3],
-                                     [4, 5, 6],
-                                     [7, 8, 9]]
-                                    )
-
-
+        self.max_ind = self.n - 1
+        
+        self.cube = np.array( [
+            np.repeat(0, self.n**2 ).reshape( self.n, self.n ),
+            np.repeat(1, self.n**2 ).reshape( self.n, self.n ),
+            np.repeat(2, self.n**2 ).reshape( self.n, self.n ),
+            np.repeat(3, self.n**2 ).reshape( self.n, self.n ),
+            np.repeat(4, self.n**2 ).reshape( self.n, self.n ),
+            np.repeat(5, self.n**2 ).reshape( self.n, self.n )
+            ] )
+        
+  
     # rotation functions:
         # 'ind' refers to rubix cube layer to rotate 
         #  ind: int, 0 <=  ind  < n 
 
     def rotate_xy(self, ind: int):
         "Rubix cube rotation on xy"
-        pass
+        # 1,3,4,2 (rotation order)
+            
+        # primary rotation
+        array_copy = copy.copy(self.cube) # before rotation
+        
+            # rotate - change array layers (refer to cube neeting)
+        self.cube[1][ind] =  array_copy[2][ind]
+        self.cube[3][ind] =  array_copy[1][ind]
+        self.cube[4][self.max_ind - ind] =  array_copy[3][ind][::-1]  
+        self.cube[2][ind] =  array_copy[4][self.max_ind - ind][::-1]  
+        
+        # rotate sides (parallel sides)
+        if ind == 0:
+            self.cube[0] = np.rot90(self.cube[0])
+        elif ind == self.n - 1:
+            self.cube[5] = np.rot90(self.cube[5], k = -1)
+            
+       
 
     def rotate_xz(self, ind: int):
         "Rubix cube rotation on xz"
-        pass
+        # 1,0,4,5 (rotation order)
+        
+        # primary rotation
+        array_copy = copy.copy(self.cube) # before rotation
+        
+            # rotate - change array layers (refer to cube neeting)
+        self.cube[1][:,ind] =  array_copy[5][:,ind]
+        self.cube[0][:,ind] =  array_copy[1][:,ind]
+        self.cube[4][:,ind] =  array_copy[0][:,ind]
+        self.cube[5][:,ind] =  array_copy[4][:,ind] 
+        
+        # rotate sides (parallel sides)
+        if ind == 0:
+            self.cube[2] = np.rot90(self.cube[2])
+        elif ind == self.n - 1:
+            self.cube[3] = np.rot90(self.cube[3], k = -1)
+        
 
     def rotate_yz(self, ind: int):
         "Rubix cube rotation on yz"
-        pass
+        # 0,3,5,2 (rotation order)
+        
+        # primary rotation
+        array_copy = copy.copy(self.cube) # before rotation
+        
+            # rotate - change array layers (refer to cube neeting)
+        self.cube[0][ind] =  array_copy[2][:, ind][::-1] 
+        self.cube[3][:, self.max_ind - ind] =  array_copy[0][ind] 
+        self.cube[5][self.max_ind - ind] =  array_copy[3][:, self.max_ind - ind][::-1] 
+        self.cube[2][:,  ind] =  array_copy[5][self.max_ind - ind] 
+        
+        # rotate sides (parallel sides)
+        if ind == 0:
+            self.cube[4] = np.rot90(self.cube[4])
+        elif ind == self.n - 1:
+            self.cube[1] = np.rot90(self.cube[1], k = -1)
+            
 
 
-    def rotate_array(self, array: nd.array, k: rot):
-        "Rotates matrix arrays"
-        return np.rot90(array, k = rot, axes = 90)
-
-
-    
 
 if __name__ == "__main__":
-    cube_test = rubix(3)
-    #print(cube_test.sides)
+    cube = rubix_logic(3)
+    
+    cube.rotate_xz(2)
+    cube.rotate_xy(0)
+    cube.rotate_yz(2)
+    cube.rotate_yz(2)
+    print(cube.cube)
 
 
